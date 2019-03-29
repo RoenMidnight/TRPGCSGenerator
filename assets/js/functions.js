7a1cef6cf87b3c8a575f00da0faceb63770e4a8d
Aeithne <40723680+Aeithne@users.noreply.github.com>
2019-03-29 18:13:25 -0300

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,10 +2,14 @@ function addPericia(element, atributo) {
 	var node = document.createElement("div");
 	node.className = "row pericia pericia" + atributo;
 	node.innerHTML = '<div class="col-1"><input type="checkbox"></div>' +
-		'<div class="col-3"><input type="text" class="bordas"></div>' +
+		'<div class="col-3"><input type="text" class="bordasfinas" style="max-width:100%"></div>' +
 		'<div class="col-2"><input type="number" step="1" class="caixaEntrada"></div>' +
 		'<div class="col-2"><input type="number" step="1" class="caixaEntrada"></div>' +
-		'<div class="col-2"><input type="number" step="1" class="caixaEntrada mod_' + atributo + '" size="3"></div>' +
+		'<div class="col-2"><input type="number" step="1" class="caixaEntrada mod_' + atributo + '" placeholder="'+ atributo + '" size="3"></div>' +
 		'<div class="col-2"><input type="number" step="1" class="caixaEntrada"></div>';
-	element.parentElement.parentElement.parentElement.insertBefore(node, element.parentElement.parentElement.nextElementSibling);
+		if (atributo == "") {
+			element.parentElement.parentElement.parentElement.insertBefore(node, element.parentElement.parentElement.parentElement.lastChild);
+		} else {
+		element.parentElement.parentElement.parentElement.insertBefore(node, element.parentElement.parentElement.nextElementSibling);
+	}
 }
